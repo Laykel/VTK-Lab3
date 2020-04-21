fbf1133c20c8aea1a6c953c30fa2f1d694dea99f
Laykel <layke.luc@gmail.com>
2020-04-21 13:18:15 +0200

    Just reading the heights file

--- colormapping.py
+++ colormapping.py
@@ -11,20 +11,40 @@ Python version: 3.7.4
 import vtk
 
 
+def read_height_matrix(filename):
+    """Read a file that has two integers on the first line (width, height) and then the values of a matrix"""
+    with open(filename, 'r') as file:
+        width, height = [int(x) for x in next(file).split()]
+        matrix = [[int(x) for x in line.split()] for line in file]
+
+    return width, height, matrix
+
+
 # Main instructions
 def main():
-    cube = vtk.vtkCubeSource()
+    colors = vtk.vtkNamedColors()
+
+    width, height, heights = read_height_matrix('altitudes.txt')
+
+    points = vtk.vtkPoints()
+
+    for i in range(width):
+        for j in range(height):
+            points.InsertNextPoint(i, j, heights[i][j])
+
+    polydata = vtk.vtkPolyData()
+    polydata.SetPoints(points)
 
     # Map data and create actors
     mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputConnection(cube.GetOutputPort())
+    mapper.SetInputData(polydata)
 
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
 
     renderer = vtk.vtkRenderer()
     renderer.AddActor(actor)
-    # renderer.SetBackground(colors.GetColor3d("Cornsilk"))
+    renderer.SetBackground(colors.GetColor3d("Cornsilk"))
 
     # Window properties
     ren_win = vtk.vtkRenderWindow()
